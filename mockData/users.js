0f229d91f0feb5082ca093eb3e749dff22fd8c5c
Abhasjaiswal <abhas.jaiswal0907@gmail.com>
2025-06-23 19:27:47 +0530

    Graph QL setup

---
+++ mockData/users.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+        id: 1,
+        name: 'Abhas Jaiswal',
+        email: 'abhas.jaiswal0907@gmail.com',
+        age: 22,
+        gender: 'male',
+        city: 'Benagluru',
+        country: 'India',
+    },
+]
+
+module.exports = users
